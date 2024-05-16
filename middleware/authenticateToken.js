51ffa75d0a8b54d2b270bc6b83479a6d0ccce877
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-05-16 20:27:48 +0200

    Login funcionando con redirección.
    Login funcionando con redirección.
    
    Prueba 1 para cargar datos del user en DashBoard

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -5,7 +5,7 @@ export function authenticateToken(req, res, next) {
 
     if (!token) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
         next();
