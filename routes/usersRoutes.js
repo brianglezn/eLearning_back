c867c02545c2e70e8d5a91025d9d7f8e060ecf9a
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-05-16 19:21:06 +0200

    Añadimos middleware
    Añadimos middleware para acceso a datos de la bd

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getAllUsers, getUserById, loginUser, registerUser } from '../controllers/usersController.js';
+import { authenticateToken } from '../middleware/authenticateToken.js';
 
 const router = express.Router();
 
-router.get('/users', getAllUsers);
-router.get('/users/:id', getUserById);
+router.get('/users', authenticateToken, getAllUsers);
+router.get('/users/:id', authenticateToken, getUserById);
 router.post('/login', loginUser);
 router.post('/register', registerUser);
 
