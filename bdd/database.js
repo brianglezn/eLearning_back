3d72191fd3cd657ea225bf693437b1ce34be5258
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-05-21 19:12:01 +0200

    prueba 5
    permitimos sin ssl

--- bdd/database.js
+++ bdd/database.js
@@ -6,39 +6,34 @@ dotenv.config();
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
-const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@elearning.exdyomm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&ssl=true`;
+const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@elearning.exdyomm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
-// Create a MongoClient instance
 const client = new MongoClient(DB_URI, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
     },
+    tls: true,
+    tlsAllowInvalidCertificates: true,
 });
 
-// Function to initiate connection with the database
 async function run() {
     try {
-        // Connect to the server
         await client.connect();
         console.log("Successfully connected to MongoDB");
 
-        // Send a ping to confirm the connection
         const pingResult = await client.db("admin").command({ ping: 1 });
         console.log("Ping to MongoDB:", pingResult);
     } catch (error) {
-        // Handle any error that occurs during the connection
         console.error("Error connecting to MongoDB:", error);
-        // Try to close the client if there is an error
         try {
             await client.close();
         } catch (closeError) {
             console.error("Error closing the connection to MongoDB:", closeError);
         }
-        throw error; // Rethrow the error to handle it further up
+        throw error;
     }
 }
 
-// Export the client, run function, and DB_NAME
 export { client, run, DB_NAME };
